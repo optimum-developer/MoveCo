278f91bd22d28fbb89753cd35af3a3d379829f7f
farzpalsingh <farz.bhullar@gmail.com>
2017-10-24 22:49:42 +0530

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,16 +35,83 @@ set :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :user,            'deploy'
+set :pass,            'TTT@123'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
 
-namespace :deploy do
+#set :ssh_options,     { user: fetch(:user),auth_methods: %w(password),password: fetch(:pass) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+## Defaults:
+# set :scm,           :git
+ set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
 
+  before :start, :make_dirs
 end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/staging`
+        puts "WARNING: HEAD is not the same as origin/staging"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  desc 'Restart Delayed Job'
+  after 'deploy:published', 'restart' do
+    invoke 'delayed_job:restart'
+  end
+
+
+  before :starting,     :check_revision
+  # after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
+
+
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,6 +9,12 @@ server '139.59.88.132',
       password: 'TTT@123'
   }
 
+set :deploy_to,       "/home/#{fetch(:user)}/production/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
