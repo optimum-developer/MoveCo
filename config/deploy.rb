d50570263893a91a7e44b8c540495c3328e70ed0
farzpalsingh <farz.bhullar@gmail.com>
2017-10-25 20:25:34 +0530

    adding vender bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'assets', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/assets', 'public/system', 'public/uploads', 'vendor/bundle')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
