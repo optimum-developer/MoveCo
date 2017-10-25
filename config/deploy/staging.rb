80897be072740f76773c4bc7ae6ba9bd090d6af3
farzpalsingh <farz.bhullar@gmail.com>
2017-10-25 18:12:07 +0530

    removing assets.digest

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -16,4 +16,3 @@ set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 
-config.assets.digest = true
