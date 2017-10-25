799863d3a585b6ebb654e38197899a32af086df4
farzpalsingh <farz.bhullar@gmail.com>
2017-10-25 19:37:56 +0530

    assts to dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'assets', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
